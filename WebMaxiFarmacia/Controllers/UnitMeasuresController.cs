c287f19f8a29341dad590f8c452410db0b64a52a
Roberto <Roberto@DESKTOP-50SERLI>
2017-08-19 00:15:11 -0600

    avances en reportes de rango

---
+++ WebMaxiFarmacia/Controllers/UnitMeasuresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebMaxiFarmacia.Models;
+
+namespace WebMaxiFarmacia.Controllers
+{
+    public class UnitMeasuresController : Controller
+    {
+        private maxifarmaciabdContext db = new maxifarmaciabdContext();
+
+        // GET: UnitMeasures
+        public ActionResult Index()
+        {
+            return View(db.UnitMeasures.ToList());
+        }
+
+        // GET: UnitMeasures/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UnitMeasure unitMeasure = db.UnitMeasures.Find(id);
+            if (unitMeasure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unitMeasure);
+        }
+
+        // GET: UnitMeasures/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UnitMeasures/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UnitMeasure unitMeasure)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UnitMeasures.Add(unitMeasure);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(unitMeasure);
+        }
+
+        // GET: UnitMeasures/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UnitMeasure unitMeasure = db.UnitMeasures.Find(id);
+            if (unitMeasure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unitMeasure);
+        }
+
+        // POST: UnitMeasures/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UnitMeasure unitMeasure)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(unitMeasure).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(unitMeasure);
+        }
+
+        // GET: UnitMeasures/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UnitMeasure unitMeasure = db.UnitMeasures.Find(id);
+            if (unitMeasure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unitMeasure);
+        }
+
+        // POST: UnitMeasures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UnitMeasure unitMeasure = db.UnitMeasures.Find(id);
+            db.UnitMeasures.Remove(unitMeasure);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebMaxiFarmacia/Models/UnitMeasure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebMaxiFarmacia.Models
+{
+    public class UnitMeasure
+    {
+        [Key]
+        public int UnitMeasureId { get; set; }
+
+        //[Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Tipo { get; set; }
+
+    
+
+        public virtual ICollection<Product> Products { get; set; }
+    
+    }
+}
